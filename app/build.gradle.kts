644d8358616c72b5fd001282364a2daaf868fcb0
jnix77 <jnix77.dev@gmail.com>
2026-02-28 09:29:34 +0100

    - version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.energomonitor.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
     }
 
     buildTypes {
